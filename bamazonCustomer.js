311dcb492761e711fa68f4a2fb2aa511bfe0d7f4
Trai Nguyen <tnguyen@at-pac.com>
2019-11-06 16:15:39 -0500

    added inquire questions about what item and how many

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,7 +18,11 @@
 
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var keys = require ("./keys.js") 
+var keys = require ("./keys.js");
+
+var saleList = [];
+var selectedItem =0;
+var selectedQuantity =0;
 
 var connection = mysql.createConnection(keys.databaseKeys);
   
@@ -26,21 +30,61 @@ connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
     queryTable();
+
 });
 
+// The first should ask them the ID of the product they would like to buy.
+// The second message should ask how many units of the product they would like to buy.
+function inquireItem() {
+    inquirer
+        .prompt([
+            {
+                name: "selectedItem",
+                type: "list",
+                message: "What item would you like to purchase?",
+                choices: saleList
+            },
+            {
+                name: "selectedQuantity",
+                type: "input",
+                message: "How many would you like to purchase?",
+                validate: function(value) {
+                    if (isNaN(value) === false) {
+                    return true;
+                    }
+                    return false;
+                }
+            }
+        ]).then(function(answer) {
+        console.log("You have selected item number: ", answer.selectedItem);
+        selectedItem = answer.selectedItem
+        console.log("Amount purchase: ", answer.selectedQuantity);
+        selectedQuantity = answer.selectedQuantity
+    });
+    
+};
 
+
+
+// This function display the items available for purchase
 function queryTable() {
     var query = connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
         for (var i = 0; i < res.length; i++) {
-            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price+ " | " + res[i].stock_quantity);
-          }
-          console.log("-----------------------------------");
-        //   console.log(res)
-        
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].price);
+            saleList.push(`${res[i].item_id}`);
+        }
+            console.log("-----------------------------------");
+            // console.log(res);
+            inquireItem();
+            if (selectedItem <= selectedQuantity) {
+                console.log("enough to sell");
+            } else {
+                console.log("out of stock");
+            }
+            
+                    
       });
-    
       // logs the actual query being run
       console.log(query.sql);
-      connection.end();
 }
