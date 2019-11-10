8c8aa98e7109802f5f1d249f72218ed663d3fd0b
Trai Nguyen <tmnguyen8@gmail.com>
2019-11-09 22:05:54 -0500

    adding folders

--- assets/js/bamazonCustomer.js
+++ assets/js/bamazonCustomer.js
@@ -1,21 +1,3 @@
-// 5. Then create a Node application called `bamazonCustomer.js`. 
-// Running this application will first display all of the items available for sale. 
-// Include the ids, names, and prices of products for sale.
-
-// 6. The app should then prompt users with two messages.
-
-//    * The first should ask them the ID of the product they would like to buy.
-//    * The second message should ask how many units of the product they would like to buy.
-
-// 7. Once the customer has placed the order, 
-// your application should check if your store has enough of the product to meet the customer's request.
-
-//    * If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
-
-// 8. However, if your store _does_ have enough of the product, you should fulfill the customer's order.
-//    * This means updating the SQL database to reflect the remaining quantity.
-//    * Once the update goes through, show the customer the total cost of their purchase.
-
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var keys = require ("./keys.js");
@@ -85,7 +67,7 @@ function queryTable() {
             saleList.push(`${res[i].item_id}`);
         }
             console.log("-----------------------------------");
-            console.log("this is sale list", saleList)
+            // console.log("this is sale list", saleList)
             // console.log(res);
             inquireItem();
           
